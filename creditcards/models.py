8ad01abee57a0db03f63a2b893e86e134fa82e37
Nathan Burnett <nathanburnett@Nathans-MacBook-Pro.local>
2019-03-01 20:28:06 -0800

    Made basic db schema, added gitignore for cache

--- creditcards/models.py
+++ creditcards/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+class Issuer(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+
+class Reward(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+
+class Card(models.Model):
+    issuer = models.ForeignKey(Issuer, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    reward = models.ForeignKey(Reward, on_delete=models.PROTECT)
+
+    class Meta:
+        unique_together = (('issuer', 'name'),)
+
